05979b466711df9e45d8916e7985536f64028446
Dan Chui <dchui108@gmail.com>
2023-08-18 18:16:23 +0900

    add Spinner and Suspense components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import { Home } from "./pages/Home";
@@ -7,19 +8,22 @@ import { Success } from "./pages/Success";
 import { Cancel } from "./pages/Cancel";
 import { Navbar } from "./components/Navbar";
 import { ShoppingCartProvider } from "./context/ShoppingCartContext";
+import SpinnerFullPage from "./components/SpinnerFullPage";
 
 function App() {
   return (
     <ShoppingCartProvider>
       <Navbar />
       <Container className="mb-4">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/store" element={<Store />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/success" element={<Success />} />
-          <Route path="/cancel" element={<Cancel />} />
-        </Routes>
+        <Suspense fallback={<SpinnerFullPage />}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/store" element={<Store />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/success" element={<Success />} />
+            <Route path="/cancel" element={<Cancel />} />
+          </Routes>
+        </Suspense>
       </Container>
     </ShoppingCartProvider>
   );
